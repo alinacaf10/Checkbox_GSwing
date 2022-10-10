6af37994530df8388d8f13532e912c99e2642463
FARHAD  NAJAFOV <farhadnajafov98.com>
2022-10-10 18:06:29 +0400

    Only one checkbox must checked

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,19 +3,38 @@ package main;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 public class Main {
 
     public static void main(String[] args) {
 	JFrame frame=new JFrame();
     JLabel l1=new JLabel("Which");
-    l1.setBounds(100,50,100,10);
+    l1.setBounds(100,50,1000,40);
     JCheckBox c1=new JCheckBox("First");
     c1.setBounds(100,100,100,50);
     JCheckBox c2=new JCheckBox("Second");
     c2.setBounds(100,150,100,50);
     JButton btn=new JButton("Submit");
     btn.setBounds(100,200,100,100);
+  c1.addItemListener(new ItemListener() {
+      @Override
+      public void itemStateChanged(ItemEvent e) {
+          if (e.getStateChange()==1) {
+              c2.setSelected(false);
+          }
+      }
+  });
+  c2.addItemListener(new ItemListener() {
+      @Override
+      public void itemStateChanged(ItemEvent e) {
+          if (e.getStateChange()==1){
+              c1.setSelected(false);
+          }
+      }
+  });
+
     btn.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
